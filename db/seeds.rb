e1262af75349531dee52efc7ec21d26728cb304d
harimohanraj89 <hari.mohanraj89@gmail.com>
2014-09-13 16:00:19 -0400

    seed file for users and questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Question.destroy_all
+
+User.create!(
+  first_name: 'Admin',
+  last_name: 'Admininstrator',
+  email: 'admin@admin.com',
+  role: 'admin',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+User.create!(
+  first_name: 'Hari',
+  last_name: 'Mohanraj',
+  email: 'hari@hari.com',
+  role: 'instructor',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+User.create!(
+  first_name: 'Jimbob',
+  last_name: 'Junior',
+  email: 'jim@bob.com',
+  role: 'student',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+Question.create!(
+  body: 'What is "this" in JavaScript?'
+)
+
+Question.create!(
+  body: 'What is the meaning of life?'
+)
+
+Question.create!(
+  body: 'Why are my boots sticky?'
+)
+
+Question.create!(
+  body: 'What is the difference between map and each in Ruby?'
+)
+
+Question.create!(
+  body: 'Can I use a Rails app to control a spaceship to fly to the moon?'
+)
+
+Question.create!(
+  body: 'How does Rails know where to find my model file without me telling it anything?'
+)
