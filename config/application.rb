86be1aec116f39237720a1c8e462d41a48e70490
Delmer Reed <delmer814@yahoo.com>
2014-09-13 16:10:01 -0400

    bower setup, angular install

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module WdiQuestionsApp
   class Application < Rails::Application
+    # Add Bower components to asset pipeline
+    config.assets.paths << Rails.root.join("vendor", "assets", "components")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
